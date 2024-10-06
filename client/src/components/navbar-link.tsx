faae87a48cdaad33485ca1620ec64656c11a1816
MarroosSK <maros1chmel@gmail.com>
2024-10-06 11:06:58 +0200

    removed transition link

--- client/src/components/navbar-link.tsx
+++ client/src/components/navbar-link.tsx
@@ -1,13 +1,12 @@
-import { useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { NavbarLinkI } from "../types/types";
 import { cn } from "../lib/utils";
-import TransitionLink from "../utils/transition-link";
 
 const NavbarLink = ({ ...link }: NavbarLinkI) => {
   const { pathname } = useLocation();
 
   return (
-    <TransitionLink
+    <NavLink
       to={link.path}
       className={cn(
         "relative group transition duration-300 text-stone-500",
@@ -17,7 +16,7 @@ const NavbarLink = ({ ...link }: NavbarLinkI) => {
       {link.title}
       <span className="text-sky-800 absolute top-0 left-1/2 w-0 h-0.5 bg-current transition-all duration-500 group-hover:left-0 group-hover:w-1/2"></span>
       <span className="text-sky-800 absolute top-0 right-1/2 w-0 h-0.5 bg-current transition-all duration-500 group-hover:right-0 group-hover:w-1/2"></span>
-    </TransitionLink>
+    </NavLink>
   );
 };
 
